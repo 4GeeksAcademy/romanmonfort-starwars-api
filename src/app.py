d21e3428783bede74cef716da688d4edbc3aa6af
Roman Monfort <137888802+romanmonfort@users.noreply.github.com>
2023-12-15 23:51:52 +0000

    all

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Planet,Character,Vehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,157 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/user/<int:user_id>', methods=['GET'])
+def handle_hello(user_id):
+    users = User.query.get(user_id)
+    serialized_users= users.serialize()
+    return jsonify({'msg':"ok","result":serialized_users}), 200
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    return jsonify({'msg': "ok", 'result': serialized_users}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user', methods=['POST'])
+def post_users():
+    body = request.get_json(silent=True)
 
-    return jsonify(response_body), 200
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información en el body'}), 400
+
+    if "email" not in body or "password" not in body or "is_active" not in body:
+        return jsonify({'msg': 'Los campos email, password y is_active son obligatorios'}), 400
+
+    new_user = User(email=body['email'], password=body['password'], is_active=body['is_active'])
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'msg': "Usuario creado exitosamente"}), 200
+
+
+
+
+
+
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    serialized_planets = [planet.serialize() for planet in planets]
+    return jsonify({'msg': "ok", 'result': serialized_planets}), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    
+    if planet:
+        serialized_planet = planet.serialize()
+        return jsonify({'msg': "ok", 'result': serialized_planet}), 200
+    else:
+        return jsonify({'msg': "Planet not found", 'result': {}}), 404
+
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    data = request.get_json()
+
+    if not data or "name" not in data:
+        return jsonify({'msg': 'Debes proporcionar el nombre del planeta'}), 400
+
+    new_planet = Planet(name=data['name'], population=data.get('population'))
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify({'msg': 'Planeta creado exitosamente', 'planet': new_planet.serialize()}), 200
+
+
+
+
+
+
+
+
+
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    serialized_characters = [character.serialize() for character in characters]
+    return jsonify({'msg': "ok", 'result': serialized_characters}), 200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Character.query.get(character_id)
+    
+    if character:
+        serialized_character = character.serialize()
+        return jsonify({'msg': "ok", 'result': serialized_character}), 200
+    else:
+        return jsonify({'msg': "Character not found", 'result': {}}), 404
+
+@app.route('/characters', methods=['POST'])
+def create_character():
+    data = request.get_json()
+
+    if not data or "name" not in data or "planet_id" not in data:
+        return jsonify({'msg': 'Debes proporcionar  el nombre del personaje y el ID del planeta de donde viene'}), 400
+
+    new_character = Character(
+        name=data['name'],
+        height=data.get('height'),
+        mass=data.get('mass'),
+        planet_id=data['planet_id']
+    )
+
+    db.session.add(new_character)
+    db.session.commit()
+
+    return jsonify({'msg': 'Personaje creado exitosamente', 'character': new_character.serialize()}), 201
+
+
+
+
+
+
+
+
+
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicle.query.all()
+    serialized_vehicles = [vehicle.serialize() for vehicle in vehicles]
+    return jsonify({'msg': "ok", 'result': serialized_vehicles}), 200
+
+@app.route('/vehicles/<int:vehicle_id>', methods=['GET'])
+def get_vehicle(vehicle_id):
+    vehicle = Vehicle.query.get(vehicle_id)
+    
+    if vehicle:
+        serialized_vehicle = vehicle.serialize()
+        return jsonify({'msg': "ok", 'result': serialized_vehicle}), 200
+    else:
+        return jsonify({'msg': "Vehicle not found", 'result': {}}), 404
+
+@app.route('/vehicles', methods=['POST'])
+def create_vehicle():
+    data = request.get_json()
+
+    if not data or "name" not in data or "type" not in data:
+        return jsonify({'msg': 'Debes proporcionar el nombre del vehículo y el tipo'}), 400
+
+    new_vehicle = Vehicle(
+        name=data['name'],
+        type=data['type']
+    )
+
+    db.session.add(new_vehicle)
+    db.session.commit()
+
+    return jsonify({'msg': 'Vehículo creado exitosamente', 'vehicle': new_vehicle.serialize()}), 201
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,15 +5,71 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=False, )
+    is_active = db.Column(db.Boolean(), unique=False, )
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+    
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), unique=True)
+    population = db.Column(db.Integer)
+    characters = db.relationship('Character', back_populates='planet')
+
+    def __repr__(self):
+        return 'Planeta {} con id {}'.format(self.name,self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+        }
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), unique=True)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planet = db.relationship('Planet', back_populates='characters')
+
+    def __repr__(self):
+        return 'Personaje {} con id {}'.format(self.name,self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "planet": self.planet.serialize(),
+        }
+    
+class Vehicle(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    type = db.Column(db.String(25))
+
+    def __repr__(self):
+        return 'Vehiculo {} con id {}'.format(self.name,self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "pilot": self.pilot.serialize(),
         }
